d142d73adf7c3ca02d46da7ed6ac0c9a45be4d1c
Kamila Bicalho Fraga <kamibicalho@gmail.com>
2023-11-09 17:55:14 -0300

    aprimoramento das funções subtrair e dividir
    agora a função subtrair fica vermelha ao resultar negativo, e a função dividir fica vermelho e negrito ao dividir por zero. Agora a calculadora também reconhece espaço em branco como o número zero.

--- calculadora.js
+++ calculadora.js
@@ -5,6 +5,10 @@ function exibirNaTela(classe, mensagem) {
     contexto.innerHTML = mensagem;
 }
 
+if (usuario == null) {
+    usuario = 'usuário'
+}
+
 let saudacao = `Bem vindo ${usuario}!`;
 exibirNaTela('.titulo', saudacao)
 
@@ -12,15 +16,35 @@ function resolverOperacoes() {
     let primeiroNumero = document.querySelector('.container__input__primeiro').value;
     let segundoNumero = document.querySelector('.container__input__segundo').value;
 
-    const somar = (numero1 = 0, numero2 = 0) => parseInt(numero1) + parseInt(numero2);
-    const subtrair = (numero1 = 0, numero2 = 0) => numero1 - numero2;
-    const multiplicar = (numero1 = 0, numero2 = 1) => numero1 * numero2;
-    const dividir = (numero1 = 0, numero2 = 1) => numero1 / numero2;
+    primeiroNumero = isNaN(parseInt(primeiroNumero)) ? 0 : primeiroNumero;
+    segundoNumero = isNaN(parseInt(segundoNumero)) ? 0 : segundoNumero;
 
     exibirNaTela('.resultado__somar', `A soma é ${somar(primeiroNumero, segundoNumero)}.`)
-    exibirNaTela('.resultado__subtrair', `A subtração é ${subtrair(primeiroNumero, segundoNumero)}.`)
+    
+    let resultadoSubtracao = subtrair(primeiroNumero, segundoNumero);
+    exibirNaTela('.resultado__subtrair', `A subtração é ${resultadoSubtracao}.`)
+    if (resultadoSubtracao < 0) {
+        document.getElementById("subtrair").style.color = "var(--cor-botoes)";
+    } else {
+        document.getElementById("subtrair").style.color = "var(--cor-texto)";
+    }
+
     exibirNaTela('.resultado__multiplicar', `A multiplicação é ${multiplicar(primeiroNumero, segundoNumero)}.`)
-    exibirNaTela('.resultado__dividir', `A divisão é ${dividir(primeiroNumero, segundoNumero)}.`)
+
+    let mensagemDivisao = `A divisão é ${dividir(primeiroNumero, segundoNumero)}.`;
+    if (segundoNumero == 0) {
+        mensagemDivisao = `Não é possível dividir por zero!`;
+        document.getElementById("dividir").style.color = "var(--cor-botoes)";
+        document.getElementById("dividir").style.fontWeight = "700";
+    } else {
+        document.getElementById("dividir").style.color = "var(--cor-texto)";
+        document.getElementById("dividir").style.fontWeight = "400";
+    }
+    exibirNaTela('.resultado__dividir', mensagemDivisao);
 
 }
 
+const somar = (numero1, numero2) => parseInt(numero1) + parseInt(numero2);
+const subtrair = (numero1, numero2) => numero1 - numero2;
+const multiplicar = (numero1, numero2) => numero1 * numero2;
+const dividir = (numero1, numero2) => numero1 / numero2;
